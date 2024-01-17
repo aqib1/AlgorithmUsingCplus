b5c0c57e2322a8a5349140465278326d47f14e48
aqib javed <aqibbutt3078@gmail.com>
2024-01-17 01:02:03 +0000

    Convert sorted array to binary tree

---
+++ bst/SortedArrayToBST.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Aqib Javed on 12/01/2025.
+//
+#include "TreeNode.h"
+#include "iostream"
+using namespace std;
+
+class SortedArrayToBST {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return bst(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode* bst(vector<int>& nums, int low, int high) {
+      if(low > high)
+        return nullptr;
+
+      int mid = low + (high - low) / 2;
+      TreeNode* root = new TreeNode(nums[mid]);
+      root->left = bst(nums, low, mid - 1);
+      root->right = bst(nums, mid + 1, high);
+      return root;
+    }
+};
