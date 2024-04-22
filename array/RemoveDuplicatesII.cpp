3863f6d6cf8db64af75e9cec7a53e3b0d67a8776
aqib javed <aqibbutt3078@gmail.com>
2024-04-22 01:02:03 +0100

    Leetcode 80 | Remove duplicates from sorted list II using cpp

---
+++ array/RemoveDuplicatesII.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Aqib Javed on 28/03/2025.
+//
+#include "iostream"
+
+using namespace std;
+
+class RemoveDuplicatesII {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= 2) {
+          return nums.size();
+        }
+        int slow = 2; int fast = 2;
+
+        while(fast < nums.size()) {
+          if(nums[slow - 2] == nums[fast]) {
+            fast++;
+          } else {
+            nums[slow++] = nums[fast++];
+          }
+        }
+
+        return slow;
+    }
+};
