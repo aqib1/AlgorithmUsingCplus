ddac4a08512916c226fadecb5085e8c5e9cdc388
aqib javed <aqibbutt3078@gmail.com>
2024-06-15 01:02:03 +0100

    Inorder successor using c++

---
+++ bst/InOrderSuccessor.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Aqib Javed on 04/01/2025.
+//
+
+#include "TreeNode.h"
+#include "iostream"
+using namespace std;
+
+class InOrderSuccessor {
+  // Time complexity O(n) + O(n) = O2(n)
+  // Space complexity O(n)
+  public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+      vector<TreeNode*> nodes;
+      inOrder(root, nodes);
+      for(int i = 0; i < nodes.size() - 1; i++) {
+        if(p->val == nodes[i]->val) {
+          return nodes[i + 1];
+        }
+      }
+      return nullptr;
+    }
+
+    void inOrder(TreeNode* root, vector<TreeNode*>& nodes) {
+        if(root == nullptr) return;
+        inOrder(root->left, nodes);
+        if(root != nullptr)
+          nodes.push_back(root);
+        inOrder(root->right, nodes);
+    }
+};
+
+
+int main() {
+  TreeNode node = TreeNode(2);
+  TreeNode left = TreeNode(1);
+  TreeNode right = TreeNode(3);
+  node.left = &left;
+  node.right = &right;
+
+  InOrderSuccessor obj = InOrderSuccessor();
+  const TreeNode* res = obj.inorderSuccessor(&node, &left);
+  cout << res->val;
+
+  return 0;
+}
