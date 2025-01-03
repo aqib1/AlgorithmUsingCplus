75c02fd85e57c8849d0f9a75a09a434571b6c9d5
aqib javed <aqibbutt3078@gmail.com>
2025-01-03 21:59:43 +0000

    Find power using pingala exponentiation algorithm OLog(n)

--- MyPow.cpp
+++ MyPow.cpp
@@ -31,8 +31,29 @@ double binaryExponentiation(const int a, const long pow) {
     return a * binaryExponentiation(a * a, (pow - 1) / 2);
 }
 
+// time complexity OLog(n)
+// space complexity O(1)
+double pingalaPower(
+    const int a,
+    const long pow
+) {
+    if (pow == 0)
+        return 1;
+    if (pow == 1)
+        return a;
+    if (pow < 0)
+        return 1 / pingalaPower(a, -pow);
+
+    const int x = pingalaPower(a, pow / 2);
+    if (pow % 2 == 0)
+        return x * x;
+    return x * x * a;
+}
+
+
 int main() {
     cout << "This is a test" << endl;
     cout << "pow(2, 4) = " << pow(2, 4) << endl;
     cout << "pow(2, 4) = " << binaryExponentiation(2, 4) << endl;
+    cout << "pow(2, 4) = " << pingalaPower(2, 4) << endl;
 }
