222ae221e61e30164303d16c550901031f6be53f
aqib javed <aqibbutt3078@gmail.com>
2025-01-05 20:37:18 +0000

    Search in BST

--- bst/SearchInBST.cpp
+++ bst/SearchInBST.cpp
@@ -10,6 +10,18 @@ using namespace std;
 class Solution {
 // Time complexity O(h)
 // Space complexity O(1)
+
+/**
+* Question:
+*
+* If you do not know the height of the BST h but you are given the
+* total number of nodes N of the BST, can you express the time
+* complexity and space complexity using N instead of h?
+*
+* Answer:
+* Best-case -> OLog(N)
+* Worst-case -> O(N)
+*/
 public:
   TreeNode* searchBST(TreeNode* root, int val) {
     while(root != nullptr) {
